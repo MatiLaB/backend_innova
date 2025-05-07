310a52d19d0e7e4d8c7b7bd697c0720af164330d
MatiLaB <matias.labbe.p@uc.cl>
2025-05-07 18:58:23 -0400

    cambio seeds y migraciones

--- src/seeders/20250505001138-demo-clinics.js
+++ src/seeders/20250505001138-demo-clinics.js
@@ -5,26 +5,64 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Clinics', [
       {
-        name: 'Clínica Bella Vida',
-        location: 'Calle Salud 123, Ciudad Belleza',
-        rating: 4.5,
-        legalStatus: 'Autorizado por SEREMI',
-        agreementNote: 'Convenio con Clinica de Los Andes',
-        procedures: ['Botox', 'Liposucción'],
-        averagePrice: '5000000',
+        name: 'Clínica Médica W',
+        location: 'Cam. El Alba 9500, Oficina 306 B, Las Condes, Santiago',
+        rating: 4.9,
+        legalStatus: 'Autorizada por el MINSAL',
+        agreementNote: '',
+        procedures: ['Lipo sin cirugía', 'Full Face', 'Double Care'],
+        averagePrice: '500000',
         yearsActive: 5,
-        facilities: 'Solo un piso'
+        facilities: 'Instalaciones modernas con estacionamiento propio',
+        link: 'https://clinicamedicaw.cl'
       },
       {
-        name: 'Estética Premium',
-        location: 'Av. Belleza 456, Ciudad Cuidado',
+        name: 'Clínica Meu',
+        location: 'Av. Apoquindo 6275, Oficina 135, Las Condes, Santiago',
         rating: 4.8,
-        legalStatus: 'Investigado por SEREMI',
+        legalStatus: 'Autorizada por la Seremi de Salud',
         agreementNote: '',
-        procedures: ['Aumento mamario', 'Peeling facial'],
-        averagePrice: '6500000',
-        yearsActive: 7,
-        facilities: 'Grandes pabellones'
+        procedures: ['Armonización facial', 'Bioestimulador de colágeno', 'Lipopapada sin cirugía'],
+        averagePrice: '600000',
+        yearsActive: 3,
+        facilities: 'Equipo especializado en técnicas brasileñas y seguimiento post-tratamiento',
+        link: 'https://clinicameu.cl'
+      },
+      {
+        name: 'Clínica ETMA',
+        location: 'Av. El Alba 2, N° 582, Chicureo, Colina, Santiago',
+        rating: 4.7,
+        legalStatus: 'Autorizada por el MINSAL',
+        agreementNote: '',
+        procedures: ['Tratamientos corporales no invasivos', 'Rejuvenecimiento de la piel'],
+        averagePrice: '450000',
+        yearsActive: 4,
+        facilities: 'Especialistas en tecnología 100% no invasiva',
+        link: 'https://clinicaetma.cl'
+      },
+      {
+        name: 'Clínica Inspira',
+        location: 'Santiago, Chile',
+        rating: 4.6,
+        legalStatus: 'Autorizada por el MINSAL',
+        agreementNote: '',
+        procedures: ['Depilación láser Soprano Ice Platinum', 'Radiofrecuencia Octopolar', 'Peeling químico facial'],
+        averagePrice: '400000',
+        yearsActive: 6,
+        facilities: 'Tecnología de última generación y atención integral',
+        link: 'https://grupoinspira.cl'
+      },
+      {
+        name: 'Clínica ES',
+        location: 'Padre Mariano 391, Oficina 506, Santiago',
+        rating: 4.5,
+        legalStatus: 'Autorizada por el MINSAL',
+        agreementNote: '',
+        procedures: ['Cirugía plástica', 'Cosmetología', 'Odontología estética'],
+        averagePrice: '700000',
+        yearsActive: 10,
+        facilities: 'Atención personalizada en cirugía plástica y estética',
+        link: 'https://clinicaes.cl'
       }
     ]);
     console.log('Seed de clínicas ejecutado')
